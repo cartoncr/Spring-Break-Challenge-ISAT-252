bafd3ad6679418214550b659dd9eb3bbab541523
MrPickles2009 <MrPickles2009@users.noreply.github.com>
2017-03-13 09:30:21 -0400

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -1,4 +1,4 @@
 var phrase = window.prompt("Type out the phrase you wish to encrypt:");
 
 //remove spaces from phrase
-document.write( phrase.replace(/\s/g, '') );
+document.write(str = str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
