10ea1f6f5abf854e12ba305b578cd69211c11163
MrPickles2009 <MrPickles2009@users.noreply.github.com>
2017-03-15 09:31:19 -0400

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -9,5 +9,10 @@ phrase = phrase.toLowerCase();
 
 //count string length as new variable 'stringLength'
 var stringLength = phrase.length;
-//Round square root of 'stringLength' to nearest whole number
-Math.round(Math.sqrt(stringLength));
+//Round square root of 'stringLength' to nearest whole number as new variable 'sqrt'
+var sqrt = Math.round(Math.sqrt(stringLength));
+
+/*
+//slice up 'newphrase' variable and output encrypted phrase words separated by spaces.
+console.log(phrase.slice(0, sqrt),phrase.slice(sqrt, sqrt*2),phrase.slice(sqrt*2, sqrt*3),phrase.slice(sqrt*3, sqrt*4),phrase.slice(sqrt*4, sqrt*5),phrase.slice(sqrt*5, sqrt*6),phrase.slice(sqrt*6, sqrt*7),phrase.slice(sqrt*8, sqrt*9));
+*/
