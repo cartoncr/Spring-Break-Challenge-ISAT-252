bedad053971b224de90398be7719c303ebefc99f
MrPickles2009 <MrPickles2009@users.noreply.github.com>
2017-03-15 09:37:38 -0400

    Update encryption.js

--- encryption.js
+++ encryption.js
@@ -13,6 +13,8 @@ var stringLength = phrase.length;
 var sqrt = Math.round(Math.sqrt(stringLength));
 
 /*
-//slice up 'newphrase' variable and output encrypted phrase words separated by spaces.
+var newPhrase = //create square table of characters using 'sqrt' value for number of columns/rows
+
+//slice up 'newPhrase' variable and output encrypted phrase words separated by spaces.
 console.log(phrase.slice(0, sqrt),phrase.slice(sqrt, sqrt*2),phrase.slice(sqrt*2, sqrt*3),phrase.slice(sqrt*3, sqrt*4),phrase.slice(sqrt*4, sqrt*5),phrase.slice(sqrt*5, sqrt*6),phrase.slice(sqrt*6, sqrt*7),phrase.slice(sqrt*8, sqrt*9));
 */
